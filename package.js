4b645dfa67694e432c90e5f23dbc11724cb2c882
Isaac Han <isaac@hi16.ca>
2017-01-23 19:17:58 -0700

    sat.js

--- package.js
+++ package.js
@@ -1,27 +1,27 @@
 
 Package.describe({
     summary: 'Cubesat: framework for Meteor in coffeescript.',
-    version: '0.4.104',
+    version: '0.6.0',
     git: 'https://github.com/i4han/cubesat.git',
     documentation: 'README.md'
 });
 
 Package.on_use( function (api) {
     var lang = 'coffee'
-    lang === 'coffee' && api.use('coffeescript@1.0.6')
-    api.use('ecmascript@0.1.6')
-    api.use('jquery@1.0.1')
+    lang === 'coffee' && api.use('coffeescript')
+    api.use('ecmascript')
+    api.use('jquery')
     api.use('iron:router@1.1.1')
-    api.use('isaac:underscore2@0.4.14')
+    api.use('isaac:underscore2@0.5.0')
     api.use('isaac:jquery-x@0.0.5')
-    api.use('isaac:style-query@0.0.3')
-    api.add_files( 'es6/settings.js',           ['client', 'server'] )
+    api.use('isaac:style-query@0.0.7')
+    api.add_files( 'js/settings.js', ['client', 'server'] )
     //api.add_files( lang + '/cubesat.'  + lang,  ['client', 'server'] )
     api.add_files( lang + '/route.'    + lang,   'server'  )
     // api.add_files( lang + '/cube.'     + lang,  ['client', 'server'] )
-    api.add_files( 'es6/cube.js',    ['client', 'server'])
-    api.add_files( 'es6/cubesat.js', ['client', 'server'])
-    api.add_files( 'es6/material.js', 'client' )
+    api.add_files( 'js/cube.js',     ['client', 'server'])
+    api.add_files( 'js/cubesat.js',  ['client', 'server'])
+    api.add_files( 'js/material.js',  'client' )
     api.export( '__',       ['client', 'server'] )
     api.export( 'db',       ['client', 'server'] )
     api.export( 'cube',     ['client', 'server'] )
