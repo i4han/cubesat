36f17d048da059d34077b92e79e35f3458db47df
Isaac Han <isaac@hi16.ca>
2017-01-26 01:18:34 -0700

    ok2

--- js/sat.js
+++ js/sat.js
@@ -297,7 +297,7 @@ fix_later__coffee_compile = function() {
 
 spawn_command = (bin, command, args, path) => {
   path && cd(path)
-  console.log('   ', __.padLeft(16, bin + command + args.join(' ')), path)
+  console.log('   ', __.padLeft(16, ([bin, command].concat(args)).join(' ')), path)
   return spawn(bin, [command].concat(args), {stdio: 'inherit'}) }
 
 meteor_packages_removed = 'autopublish insecure'.split(' ');
