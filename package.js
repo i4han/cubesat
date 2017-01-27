38174a1c77d0bd5edee63a6f1020ab1c64ee8cbf
Isaac Han <isaac@hi16.ca>
2017-01-27 11:33:01 -0700

    start chart

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
     summary: 'Cubesat: framework for Meteor in coffeescript.',
-    "version": "0.6.30",
+    "version": "0.6.31",
     git: 'https://github.com/i4han/cubesat.git',
     documentation: 'README.md'
 });
