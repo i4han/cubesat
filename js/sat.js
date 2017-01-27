3947a3ea2ed821777fbd0e8da78ae8537aab91b9
Isaac Han <isaac@hi16.ca>
2017-01-27 00:36:02 -0700

    again

--- js/sat.js
+++ js/sat.js
@@ -148,8 +148,8 @@ const cp = (s, t) => fs.createReadStream(s).pipe(fs.createWriteStream(t))
 
 const spawn_command = (bin, command, args, path) => {
   path && cd(path)
-  console.log(' ', ([bin, command].concat(args)).join(' '))
-  console.log(' ', path)
+  console.log('   ', ([bin, command].concat(args)).join(' '))
+  console.log('   ', path)
   return spawn(bin, [command].concat(args), {stdio: 'inherit'}) }
 
 var Settings, __RmCoffee_paths, __commands, __func, __rmdir, __start_up, _tagLine
